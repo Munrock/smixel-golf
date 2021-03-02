74dc54633a4588f7691f69dd9e65aaf9081e4ed5
Steve Munro <munrock@gmail.com>
2021-03-02 14:58:00 +0800

    testing works

--- tests/toon.test.js
+++ tests/toon.test.js
@@ -1,6 +1,20 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-describe('Toon', ()=>{
-    test('Our first test', () => {
-        throw new Error('something failed');
+beforeAll(async () => {
+    return await mongoose.connect(process.env.MONGODB_ADDRESS, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+});
+
+afterAll(()=>{
+    mongoose.connection.close();
+});
+
+describe('Toon', () => {
+    test('should exist', () => {
+        expect(0).toBe(0);
     });
 });
