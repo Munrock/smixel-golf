a9ce476e31148193826cdc8b68447576c4e56bb8
Steve Munro <munrock@gmail.com>
2021-03-02 19:04:45 +0800

    pronoun tests

--- tests/toon.test.js
+++ tests/toon.test.js
@@ -37,6 +37,17 @@ describe('Toon', () => {
         expect(testtoon.data._id).toBeDefined();
     });
 
+    test('toon pronouns', async () => {
+        const testtoon = await Toon.getFromPlayerId('12345654321');
+
+        const string = "name is name0, nick is nick0, full name is full0, :: line break sub0 vtb0 nick0, this sentence is about obj0, the championship is pop0, it is poa0 championship, sub0 vtb0 the best.";
+
+        const processed = testtoon.pronouner.encode(string);
+
+        expect(processed).toEqual(["name is Becky, nick is The Man, full name is Becky Lynch, ", " line break they are The Man, this sentence is about them, the championship is theirs, it is their championship, they are the best."]);
+    });
+
+
     test('edit toon data', async () => {
         const testtoon = await Toon.getFromPlayerId('12345654321');
         testtoon.data.name.nick = "The Lasskicker";
@@ -50,6 +61,18 @@ describe('Toon', () => {
         expect(updatedtoon.data.pronouns).toBe("f");
         expect(updatedtoon.data._id).toBeDefined();
     });
+
+
+    test('toon pronouns again', async () => {
+        const testtoon = await Toon.getFromPlayerId('12345654321');
+
+        const string = "name is name0, nick is nick0, full name is full0, :: line break sub0 vtb0 nick0, this sentence is about obj0, the championship is pop0, it is poa0 championship, sub0 vtb0 the best.";
+
+        const processed = testtoon.pronouner.encode(string);
+
+        expect(processed).toEqual(["name is Becky, nick is The Lasskicker, full name is Becky Lynch, ", " line break she is The Lasskicker, this sentence is about her, the championship is hers, it is her championship, she is the best."]);
+    });
+
 
     test('delete a toon', async () => {
         const testtoon = await Toon.getFromPlayerId('12345654321');
