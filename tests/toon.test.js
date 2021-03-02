cf55c20a0d376b205542202fcdb183e57f07433e
Steve Munro <munrock@gmail.com>
2021-03-02 17:28:06 +0800

    test Toon update

--- tests/toon.test.js
+++ tests/toon.test.js
@@ -17,23 +17,38 @@ afterAll(() => {
 describe('Toon', () => {
     test('create a toon', async () => {
         const testtoon = await Toon.make({
-            name: { name: "Becky", nick: "The Man", full: "Becky Lynch", },
-            pronouns: "f",
+            name: { name: "Becky", nick: "The Man", full: "Becky Lynch", }
         }, '12345654321');
+
         expect(testtoon.data.name.name).toBe("Becky");
         expect(testtoon.data.name.nick).toBe("The Man");
         expect(testtoon.data.name.full).toBe("Becky Lynch");
-        expect(testtoon.data.pronouns).toBe("f");
+        expect(testtoon.data.pronouns).toBe("np");
         expect(testtoon.data._id).toBeDefined();
     });
 
     test('retrieve a toon', async () => {
         const testtoon = await Toon.getFromPlayerId('12345654321');
+
         expect(testtoon.data.name.name).toBe("Becky");
         expect(testtoon.data.name.nick).toBe("The Man");
         expect(testtoon.data.name.full).toBe("Becky Lynch");
-        expect(testtoon.data.pronouns).toBe("f");
+        expect(testtoon.data.pronouns).toBe("np");
         expect(testtoon.data._id).toBeDefined();
+    });
+
+    test('edit a toon data', async () => {
+        const testtoon = await Toon.getFromPlayerId('12345654321');
+        testtoon.data.name.nick = "The Lasskicker";
+        testtoon.data.pronouns = "f";
+        await testtoon.save();
+        const updatedtoon = await Toon.getFromPlayerId('12345654321');
+
+        expect(updatedtoon.data.name.name).toBe("Becky");
+        expect(updatedtoon.data.name.nick).toBe("The Lasskicker");
+        expect(updatedtoon.data.name.full).toBe("Becky Lynch");
+        expect(updatedtoon.data.pronouns).toBe("f");
+        expect(updatedtoon.data._id).toBeDefined();
     });
 
     test('delete a toon', async () => {
