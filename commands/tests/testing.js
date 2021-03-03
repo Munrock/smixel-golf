b0cfb37e65bfad3bfe36385a94bfa7632e835bd7
Steve Munro <munrock@gmail.com>
2021-03-03 11:32:31 +0800

    add testing command

---
+++ commands/tests/testing.js
@@ -0,0 +1,38 @@
+const { Command } = require('discord.js-commando');
+
+const prompt = require('./../lib/prompt');
+
+module.exports = class ActivityCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'testing',
+            //aliases: [],
+            group: 'tests',
+            memberName: 'testing',
+            description: 'test discordjs stuff',
+            ownerOnly: true,   
+            // args: [
+            //     {
+            //         key: 'search',
+            //         prompt: `Character Name?`,
+            //         type: 'string',
+            //         default: ''
+            //     }
+            // ],
+        });
+    }
+
+    async run(msg, { search }) {
+       
+        msg.reply('say something...');
+
+        const something = await prompt(
+            msg,
+            msg.author.id,
+            'nothing was said'
+        );
+
+        msg.say(something);
+    }
+};
+
