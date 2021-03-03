c218bdc38d1918a737570a66198ed4bb64f6d04c
Steve Munro <munrock@gmail.com>
2021-03-03 22:46:43 +0800

    prompt has accepted responses list and a default

--- discord/prompt.js
+++ discord/prompt.js
@@ -1,10 +1,19 @@
 
-const textPrompt = async function (msg, responderIds, defaultResponse) {
+const textPrompt = async function (msg, responderIds, defaultResponse, acceptedResponses = false) {
 
     try {
         //filters valid responses - ones from the target user
         const filter = response => {
-            return responderIds.includes(response.author.id);
+            const responderMatch = responderIds.includes(response.author.id);
+            let responseMatch;
+            if(!!acceptedResponses){
+                acceptedResponses.push('"');
+                repsonseMatch = acceptedResponses.includes(response.content)
+            }else{
+                //any response accepted
+                responseMatch = true;
+            }
+            return (responderMatch && responseMatch);
         };
         //listen to messages until one passes the filter
         const validResponse = await msg.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] });
