5a910cb01d4739bbc3ceeb93cf8084a653ee38e0
Steve Munro <munrock@gmail.com>
2021-03-04 15:47:44 +0800

    consolidate tests that use a DB connection

--- tests/mongo.test.js
+++ tests/mongo.test.js
@@ -1,19 +1,30 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Toon = require('./../lib/toon');
+const Player = require('./../lib/player');
+const GameData = require('./../lib/game-data');
 
 beforeAll(async () => {
-    return await mongoose.connect(process.env.MONGODB_ADDRESS, {
+    await mongoose.connect(process.env.MONGODB_ADDRESS, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-         useFindAndModify: false 
+        useFindAndModify: false 
     });
+    await Toon.make({
+        name: { name: "Becky", nick: "The Man", full: "Becky Lynch", }
+    }, 'gameplayertestid1');
 
 });
 
-afterAll(() => {
+afterAll(async () => {
+    const testtoon = await Toon.getFromPlayerId('gameplayertestid1');
+    await testtoon.destroy();
+    const othertoon = await Toon.getFromPlayerId('gameplayertestid2');
+    await othertoon.destroy();
+
     mongoose.connection.close();
 });
+
 
 describe('Toon', () => {
     test('create a toon', async () => {
@@ -81,6 +92,30 @@ describe('Toon', () => {
         await testtoon.destroy();
         const deletedtoon = await Toon.getFromId(ttid);
         expect(deletedtoon).toBe(false);
+    });
+
+});
+
+describe('Player', () => {
+
+    test('retrieve a toon from player', async () => {
+        const player = new Player('gameplayertestid1');
+
+        const toon = await player.toon();
+        expect(toon.data.name.full).toBe("Becky Lynch");
+    });
+
+
+    test('set player focus on another toon', async () => {
+        const player = new Player('gameplayertestid1');
+        const othertoon = await Toon.make({
+            name: { name: "Charlotte", nick: "The Queen", full: "Charlotte Flair", }
+        }, 'gameplayertestid2');
+
+        player.focusToon(othertoon);
+    
+        const toon = await player.toon();
+        expect(toon.data.name.full).toBe("Charlotte Flair");
     });
 
 });
