1dcd4e9828af978dbd478ed47c04df37eb349bcf
Steve Munro <munrock@gmail.com>
2021-03-16 22:57:36 +0800

    add random stat assignment to toon creator

--- discord/procedure/toon/create.js
+++ discord/procedure/toon/create.js
@@ -3,7 +3,7 @@ const Toon = require('./../../../lib/toon');
 
 async function create_toon(msg) {
     const playerId = msg.author.id;
-    const playerName = msg.member.nickname?msg.member.nickname:msg.author.username;
+    const playerName = msg.member.nickname ? msg.member.nickname : msg.author.username;
 
     //need name x3, owner, prononouns, player id
     let data = {
@@ -14,7 +14,7 @@ async function create_toon(msg) {
         },
         pronouns: "np",
     };
-    
+
     //get the pronouns
     await msg.reply([
         "answer a few questions to create your character!",
@@ -26,8 +26,8 @@ async function create_toon(msg) {
         "4 it🤖",
     ].join("\r\n"));
 
-    constPronounResponse = await prompt(msg, [playerId], "1", ['1','2','3','4']);
-    data.pronouns = ['np','f','m','ns'][parseInt(constPronounResponse)-1];
+    constPronounResponse = await prompt(msg, [playerId], "1", ['1', '2', '3', '4']);
+    data.pronouns = ['np', 'f', 'm', 'ns'][parseInt(constPronounResponse) - 1];
 
     //get the names
     await msg.reply([
@@ -45,7 +45,40 @@ async function create_toon(msg) {
     await msg.say(`Nickname [${data.name.name}]:`);
     data.name.nick = await prompt(msg, [playerId], data.name.name);
 
-    return await Toon.make(data, playerId);
+    const toon = await Toon.make(data, playerId);
+
+    const unassigned = [
+        'physical/agility',
+        'physical/fortitude',
+        'physical/might',
+        'mental/learning',
+        'mental/logic',
+        'mental/perception',
+        'mental/will',
+        'social/deception',
+        'social/persuasion',
+        'social/presence',
+    ];
+
+    const values = [
+        4,3,3,2,2,1,1,0,0,0
+    ];
+
+    for (let i = values.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [values[i], values[j]] = [values[j], values[i]];
+    }
+
+    while(values.length>0){
+        const assign = unassigned.pop().split('/');
+        const value = values.pop();
+        toon.data.stats.base[assign[0]][assign[1]] = value;
+    }
+
+    toon.save();
+
+    return toon;
+    
 }
 
 module.exports = create_toon;
