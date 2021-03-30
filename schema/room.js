cda021cc6d2237179f478e779e694c4020a873ba
Steve Munro <munrock@gmail.com>
2021-03-30 11:06:59 +0800

    room.js

--- schema/room.js
+++ schema/room.js
@@ -3,15 +3,36 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const roomSchema = new Schema({
-    nodeType: { type: String, default: 'middle', enum: ['start','end','middle']},
+    nodeType: { type: String, default: 'middle', enum: ['start', 'end', 'middle'] },
     canBeRandom: { type: Boolean, default: true },
     biome: { type: String, default: 'plains' },
     challenge: {
-       difficulty: { type: Number, default: 3 },
-       tags: { type: [String], default: ['misc']},
+        difficulty: { type: Number, default: 3 },
+        tags: { type: [String], default: ['golf club'] },
     },
-    description: { type: String, default: 'No Description'},
-    narrative: { type: String, default: 'The ball lands in an undefined location.'},
+    description: { type: String, default: 'No Description' },
+    narrative: {
+        arrival: { type: String, default: 'The ball lands in an undefined location.' },
+        success: { type: String, default: 'name0 hits the ball clean and crisp.' },
+        failure: { type: String, default: 'name0 scuffs the shot.  It veers wildly and bounces off a tree.' },
+    },
+    exits: {
+        success: {
+            exitType: { type: String, default: 'random', enum: ['random', 'link'] },
+            link: {
+                type: Schema.Types.ObjectId,
+                ref: 'Room'
+            },
+        },
+        failure: {
+            exitType: { type: String, default: 'random', enum: ['random', 'link'] },
+            link: {
+                type: Schema.Types.ObjectId,
+                ref: 'Room'
+            }
+        },
+    },
+    
 });
 
 
